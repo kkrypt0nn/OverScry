3233e6a3ae113fecc7af897e9da6af94d54791d9
Krypton <root@krypton.ninja>
2025-04-14 19:02:04 +0200

    chore: Smaller tab size for Ace editor

--- web/online/ace_editor.js
+++ web/online/ace_editor.js
@@ -1,6 +1,10 @@
 var editor = ace.edit("editor");
 editor.setTheme("ace/theme/tomorrow_night");
 editor.getSession().setMode("ace/mode/yaml");
+editor.setOptions({
+  tabSize: 2,
+  useSoftTabs: true,
+});
 editor.getSession().setValue(`version: "0.0.7-dev"
 author: "Krypton (@kkrypt0nn)"
 description: "A query to get apartments with 2 levels"
