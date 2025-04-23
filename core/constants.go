ffd609e44dd26163049a28325bb4892c5bf34e3d
Krypton <root@krypton.ninja>
2025-04-23 20:37:27 +0200

    chore: Bump version

--- core/constants.go
+++ core/constants.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name    = "OverScry"
-	Version = "0.0.11-dev"
+	Version = "0.0.12-dev"
 	Author  = "Krypton (root@krypton.ninja)"
 	Website = "https://overscry.krypton.ninja"
 )
